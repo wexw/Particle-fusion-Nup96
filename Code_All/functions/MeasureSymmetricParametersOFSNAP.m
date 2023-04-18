fe869419df1b1ef49c648e4ee788ad1b9dbb82e4
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-04-18 12:15:42 +0200

    Delete Code_All/functions directory

--- Code_All/functions/MeasureSymmetricParametersOFSNAP.m
+++
@@ -1,3 +0,0 @@
-     [rd_Top,   Rr_Top,Theta_Top,Phi_Top] = MeasureSymmetricParameters(top);
-    [rd_Bottom,   Rr_B,Theta_B,Phi_B] = MeasureSymmetricParameters(bottom);
- 
--- Code_All/functions/filter_outliers_Density.m
+++
@@ -1,8 +0,0 @@
-function [Ring_pure,Ring_No]=filter_outliers_Density(Ringtofilter,index)
-density=Ringtofilter(:,5);
-Boudary=index*mean(density);
-position=find(density>=Boudary);
-Ring_pure=Ringtofilter(position,:);
-positionNo=find(density<Boudary);
-Ring_No=Ringtofilter(positionNo,:);
-end
